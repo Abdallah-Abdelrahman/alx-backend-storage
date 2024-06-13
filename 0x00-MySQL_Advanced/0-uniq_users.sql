6a622cc98e6e539c81538116b5a6ceb5844e24b9
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-06-13 17:40:13 +0300

    Debug 0x00-0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,18 @@
 -- creates a table users following these requirements
 CREATE TABLE IF NOT EXISTS users (
-    id INT PRIMARY KEY AUTOINCREMENT,
-    email VARCHAR(255) NOT NULL UNIQUE,
+    -- `id` is an integer that auto increments and serves as the primary key
+    id INT AUTO_INCREMENT,
+
+    -- `email` is a string of up to 255 characters that must be unique and not null
+    email VARCHAR(255) NOT NULL,
+
+    -- `name` is a string of up to 255 characters
     name VARCHAR(255),
+
+    -- Define `id` as the primary key
+    PRIMARY KEY (id),
+
+    -- Ensure that `email` is unique across all records
+    UNIQUE (email)
 );
 
