7fadc64fffcaf3abf5ddceaf1be9e4d3d86b5407
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-06-13 17:38:53 +0300

    Update id type

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- creates a table users following these requirements
 CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INT PRIMARY KEY AUTOINCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
+    name VARCHAR(255),
 );
 
