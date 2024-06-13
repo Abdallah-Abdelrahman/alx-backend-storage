80d9dc190496930906dd95d82afdad4107342227
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-06-13 18:49:47 +0300

    Create view `need_meeting`

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -8,8 +8,8 @@
 -- a, INT
 -- b, INT
 -- And returns a / b or 0 if b == 0
-DELIMITER $$
-DROP FUNCTION IF EXISTS SafeDiv
+DELIMITER $$;
+DROP FUNCTION IF EXISTS SafeDiv;
 CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS FLOAT
 BEGIN
 	IF b = 0 THEN
