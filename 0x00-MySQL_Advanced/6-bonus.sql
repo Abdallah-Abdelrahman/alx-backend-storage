aa7ee8475e0683aeee0f31ed7b3fcfb0b0dd4f1e
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-06-15 00:59:20 +0300

    Debug 0x01-6

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,27 +1,28 @@
--- script that creates a stored procedure AddBonus that adds a new correction for a student.
+-- script that creates a stored PROCEDURE AddBonus that adds a NEW correction FOR a student.
 -- 
 -- Requirements:
 -- 
--- Procedure AddBonus is taking 3 inputs (in this order):
--- user_id, a users.id value (you can assume user_id is linked to an existing users)
--- project_name, a new or already exists projects - if no projects.name found in the table, you should create it
--- score, the score value for the correction
--- Context: Write code in SQL is a nice level up!
-DELIMITER $$;
-CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
-BEGIN
+-- Procedure AddBonus IS taking 3 inputs (IN this order):
+-- user_id, a users.id value (you can assume user_id IS linked TO an existing users)
+-- project_name, a NEW OR already EXISTS projects - IF no projects.name found IN the TABLE, you should CREATE it
+-- score, the score value FOR the correction
+-- Context: Write code IN SQL IS a nice level up!
+DELIMITER $$
+
+CREATE PROCEDURE AddBonus (user_id INT, project_name VARCHAR(255), score INT)
+    BEGIN
         DECLARE project_id INT;
 
-        -- check if the project already exists
-        SELECT id INTO project_id FROM projects WHERE name = project_name;
+        SELECT id INTO @project_id
+        WHERE name = project_name;
 
-        -- if the project does not exist, create it
-        IF project_id IS NULL THEN
-                INSERT INTO projects (name) VALUES (project_name);
-                SET project_id = LAST_INSERT_ID();
+        IF project_id IS NULL then
+            INSERT INTO projects (name) VALUES (name); 
+            SET @project_id = LAST_INSERT_ID();
         END IF;
 
-        -- Add the new correction
-        INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
-END;$$
-DELIMITER ;$$
+        -- add new correction
+        INSERT INTO corrections (user_id, project_id, score)
+    END$$
+
+DELIMITER ;
