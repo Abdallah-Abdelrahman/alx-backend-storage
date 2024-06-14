fdeaa4707ce01e6c5799b5d3b86815369c2ba324
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-06-14 23:32:59 +0300

    Debug 0x06-5

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -7,9 +7,8 @@ DELIMITER $$
 CREATE TRIGGER `validate_email` AFTER UPDATE ON users
 FOR EACH ROW
 BEGIN
-    IF OLD.email <> NEW.email THEN
-        UPDATE users
-        SET valid_email = 0;
+    IF OLD.email != NEW.email THEN
+        SET NEW.valid_email = 0;
     END IF;
 END$$
 
