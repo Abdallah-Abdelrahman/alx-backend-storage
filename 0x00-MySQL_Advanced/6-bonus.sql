e73b181604526ec08bd207f8541d17cdb19add14
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-06-15 01:09:10 +0300

    Debug 0x07-6

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -13,10 +13,10 @@ CREATE PROCEDURE AddBonus (user_id INT, project_name VARCHAR(255), score INT)
     BEGIN
         DECLARE project_id INT;
 
-        SELECT id INTO @project_id FROM projects
+        SELECT id INTO project_id FROM projects
         WHERE name = project_name;
 
-        IF @project_id IS NULL then
+        IF project_id IS NULL then
             INSERT INTO projects (name) VALUES (project_name); 
             SET @project_id = LAST_INSERT_ID();
         END IF;
