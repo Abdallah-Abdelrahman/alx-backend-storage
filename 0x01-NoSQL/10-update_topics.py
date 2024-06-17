edbac8abfdaebb47952c47dbdc32241bcffdf3a2
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-06-17 19:58:05 +0300

    Debug 0x01-10

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -6,4 +6,4 @@ def update_topics(mongo_collection, name, topics):
     '''changes all topics of a school document based on the name'''
     query = {'name': name}
     update = {'$set': {'topics': topics}}
-    mongo_collection.update_one(query, update)
+    mongo_collection.update_many(query, update)
