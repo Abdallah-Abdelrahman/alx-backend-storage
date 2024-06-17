4bedf632fa057480384d17c734881fa162a326c7
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-06-17 22:28:28 +0300

    Disply the top 10 of the most present IPs in the collection `nginx`

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+'''script that provides some stats about Nginx logs stored in MongoDB'''
+from pymongo import MongoClient
+
+
+METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+if __name__ == '__main__':
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_coll = client.logs.nginx
+    out = f'{nginx_coll.count_documents({})} logs\nMethods:\n'
+
+    for m in METHODS:
+        count = nginx_coll.count_documents({'method': m})
+        out += f'\tmethod {m}: {count}\n'
+    count = nginx_coll.count_documents({'method': 'GET', 'path': '/status'})
+    out += f'{count} status check\nIPS:\n'
+    pipelines = [
+            {
+                '$group': {'_id': '$ip', 'count': {'$count': {}}}
+            },
+            {
+                '$sort': {'count': -1}
+            },
+            {
+                '$limit': 10
+            }
+    ]
+    # Equavelent aggregation in SQL(it's even harder -_-)
+    '''
+    SELECT ip, COUNT(*) AS count FROM nginx
+
+    GROUP BY ip
+    ORDER BY count DESC;
+    '''
+    for ip in nginx_coll.aggregate(pipelines):
+        out += f"\t{ip.get('_id')}: {ip.get('count')}\n"
+    print(out, end='')
