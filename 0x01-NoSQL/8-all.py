bb6689f316a457abcd32ad381f266e6c81690caf
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-06-17 17:53:55 +0300

    Debug 0x02-9

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -5,4 +5,4 @@ from typing import List
 
 def list_all(mongo_collection) -> List:
     '''lists all documents in a collection'''
-    return [doc for doc in mongo_collection.find({})]
+    return mongo_collection.find({})
--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 '''Module defines `insert_school` function'''
 from typing import Dict
-from pymongo.collection import Collection
 from bson import ObjectId
 
 
-def insert_school(mongo_collection: Collection, **kwargs: Dict) -> ObjectId:
+def insert_school(mongo_collection, **kwargs: Dict) -> ObjectId:
     '''inserts a new document in a collection based on kwargs'''
     return mongo_collection.insert_one(kwargs).inserted_id
