f5e5dc05196a7c69527301132e1c8d7270e3abe8
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-06-17 17:38:35 +0300

    Insert one document

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,8 @@
+'''Module defines `list_all` function'''
+from typing import Dict
+from pymongo.collection import Collection
+
+
+def insert_school(mongo_collection: Collection, **kwargs: Dict) -> str:
+    '''inserts a new document in a collection based on kwargs'''
+    return mongo_collection.insert_one(kwargs).inserted_id
