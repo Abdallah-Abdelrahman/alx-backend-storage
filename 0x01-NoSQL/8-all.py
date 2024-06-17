68e183b4e2a9f60cafc0fcdcfee781d05ed9248a
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-06-17 17:52:38 +0300

    Debug 0x02-8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 '''Module defines `list_all` function'''
 from typing import List
-from pymongo.collection import Collection
 
 
-def list_all(mongo_collection: Collection) -> List:
+def list_all(mongo_collection) -> List:
     '''lists all documents in a collection'''
     return [doc for doc in mongo_collection.find({})]
