26262437216aa2098728933c24bebb99c9c29f33
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-06-17 17:50:36 +0300

    Debug 0x01-8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 '''Module defines `list_all` function'''
+from typing import List
 from pymongo.collection import Collection
-from pymongo.cursor import Cursor
 
 
-def list_all(mongo_collection: Collection) -> Cursor:
+def list_all(mongo_collection: Collection) -> List:
     '''lists all documents in a collection'''
-    return mongo_collection.find({})
+    return [doc for doc in mongo_collection.find({})]
--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-'''Module defines `list_all` function'''
+'''Module defines `insert_school` function'''
 from typing import Dict
 from pymongo.collection import Collection
+from bson import ObjectId
 
 
-def insert_school(mongo_collection: Collection, **kwargs: Dict) -> str:
+def insert_school(mongo_collection: Collection, **kwargs: Dict) -> ObjectId:
     '''inserts a new document in a collection based on kwargs'''
     return mongo_collection.insert_one(kwargs).inserted_id
