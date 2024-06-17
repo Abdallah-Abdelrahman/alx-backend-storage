46c71f41a64b34c2f9897ccfb346db39651ea838
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-06-17 17:32:02 +0300

    Return list of all documents

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python3
-''' '''
+'''Module defines `list_all` function'''
+from pymongo.collection import Collection
+from pymongo.cursor import Cursor
+
+
+def list_all(mongo_collection: Collection) -> Cursor:
+    '''lists all documents in a collection'''
+    return mongo_collection.find({})
