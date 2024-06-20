c097ae259f61aacda309a43689e16dff55b035b7
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-06-20 05:57:35 +0300

    Debug 0x02

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -9,42 +9,44 @@ from functools import wraps
 local_redis = redis.Redis()
 
 
-def cache_result(expiration: int = 1):
+def cache_result(fn):
     '''Decorator to cache the result of a function,
     for a given expiration time.
     '''
-    def decorator(func: Callable) -> Callable:
-        '''inner wrapper'''
-        @wraps(func)
-        def wrapper(url: str) -> str:
-            '''The real wrapper'''
-            # Check if the result is in cache
-            cached_result = local_redis.get(f"cache:{url}")
-            if cached_result:
-                return cached_result.decode('utf-8')
-
-            # Call the function and get the result
-            result = func(url)
-
-            # Cache the result with the given expiration time
-            local_redis.setex(f"cache:{url}", expiration, result)
-            return result
-        return wrapper
-    return decorator
-
-
-def track_access_count(func: Callable) -> Callable:
-    '''Decorator to track how many times a URL was accessed.'''
-    @wraps(func)
+    @wraps(fn)
     def wrapper(url: str) -> str:
-        '''wrapper function to increment count'''
-        # Increment the access count for the URL
-        local_redis.incr(f"count:{url}")
-        return func(url)
+        '''The real wrapper'''
+        # Check if the result is in cache
+        k = 'count:{}'.format(url)
+        cached_result = local_redis.get(k)
+        if cached_result:
+            return cached_result.decode('utf8')
+
+        # Call the function and get the result
+        result = fn(url)
+
+        # Cache the result with the expiry of 10 seconds
+        local_redis.setex('count:{}'.format(url), 10, result)
+        return result
     return wrapper
 
 
-@cache_result(expiration=10)
+def track_access_count(fn: Callable) -> Callable:
+    '''Decorator to track how many times a URL was accessed.'''
+    @wraps(fn)
+    def wrapper(url: str) -> str:
+        '''wrapper function to increment count'''
+        # Increment the access count for the URL
+        k = f"count:{url}"
+        if not local_redis.get(k):
+            local_redis.set(k, 1)
+        else:
+            local_redis.incr(k)
+        return fn(url)
+    return wrapper
+
+
+@cache_result
 @track_access_count
 def get_page(url: str) -> str:
     '''Fetch the HTML content of a URL and return it.'''
